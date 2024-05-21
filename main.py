bff2e62d77774b1c27cccab178c0cde08e2ea18b
chirivilorenzo <chirivilorenzo@gmail.com>
2024-05-21 16:54:28 +0200

    aggiunto percorso e animazione al personaggio

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import pygame
+import random
 from sys import exit
 from settings import *
 from player import Player
-from obstacle import Obstacle
+from base import Base
 
 
 def collision_sprite():
-    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
-        for obstacle in obstacle_group:
-            if player.sprite.rect.colliderect(obstacle.rect):
+    if pygame.sprite.spritecollide(player.sprite, base_group, False):
+        for base in base_group:
+            if player.sprite.rect.colliderect(base.rect):
                 if player.sprite.gravity > 0:
-                    player.sprite.rect.bottom = obstacle.rect.top
-                    player.sprite.gravity = 0                    
+                    player.sprite.rect.bottom = base.rect.top
+                    player.sprite.gravity = 0
+                    player.sprite.on_ground = True              
 
 
 
@@ -24,9 +26,21 @@ frame_surf = pygame.image.load('Assets/Terrain/wood_frame.png').convert_alpha()
 floor_surf = pygame.image.load('Assets/Terrain/dirt_floor.png').convert_alpha()
 
 #obstacles
-obstacle_group = pygame.sprite.Group()
-obstacle_group.add(Obstacle('gray_base_1'))
-obstacle_group.add(Obstacle('gray_base_2'))
+colors = ["gray", "orange", "brown"]
+
+base_group = pygame.sprite.Group()
+base_group.add(Base('1', random.choice(colors)))
+base_group.add(Base('2', random.choice(colors)))
+base_group.add(Base('3', random.choice(colors)))
+base_group.add(Base('4', random.choice(colors)))
+base_group.add(Base('5', random.choice(colors)))
+base_group.add(Base('6', random.choice(colors)))
+base_group.add(Base('7', random.choice(colors)))
+base_group.add(Base('8', random.choice(colors)))
+base_group.add(Base('9', random.choice(colors)))
+base_group.add(Base('10', random.choice(colors)))
+base_group.add(Base('11', random.choice(colors)))
+base_group.add(Base('12', random.choice(colors)))
 
 player = pygame.sprite.GroupSingle()
 player.add(Player())
@@ -38,10 +52,10 @@ while True:
             exit()
 
     SCREEN.blit(bg_surf, (0,0))
-    SCREEN.blit(floor_surf, (0,335))
+    SCREEN.blit(floor_surf, (0,FLOOR))
     SCREEN.blit(frame_surf, (0,0))
     
-    obstacle_group.draw(SCREEN)
+    base_group.draw(SCREEN)
     player.draw(SCREEN)
     player.update()
 
